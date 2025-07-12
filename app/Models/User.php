62e4bd12a9fed1be0aa92b4ae3a97ab7cc86696a
twixAHoy <leigh.h.miller@gmail.com>
2025-07-12 12:40:12 -0400

    Updating Talk Factory

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function talks(){
+        return $this->hasMany(Talk::class);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Talk;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()
+            ->has(Talk::factory()->count(5))
+            ->create([
+                'name' => 'Leigh Wills',
+                'email' => 'test@example.com',
+            ]);
     }
 }
